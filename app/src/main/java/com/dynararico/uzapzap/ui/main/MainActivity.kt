0e0891788031368d69e3c8e68c09b9b6492bfe15
logonrmlocal <dynara.oliveira@gmail.com>
2018-12-17 21:16:33 -0200

    xxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- app/src/main/java/com/dynararico/uzapzap/ui/main/MainActivity.kt
+++ app/src/main/java/com/dynararico/uzapzap/ui/main/MainActivity.kt
@@ -1,12 +1,17 @@
-package com.dynararico.uzapzap
+package com.dynararico.uzapzap.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dynararico.uzapzap.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
     }
 }
